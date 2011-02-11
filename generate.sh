acf03d357a9244241b0a627f43fec05982c806bd
David Montag <david.montag@neotechnology.com>
2011-02-11 00:51:45 -0800

    Rewrote it to use the maven ant tasks instead of ivy.

--- generate.sh
+++ generate.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-for version in {1.2.M01,1.2.M02,1.2.M03,1.2.M04,1.2.M05,1.2.M06,1.2,1.3.M01}; do
-  ant generate-test-graph -Dneo4j.version=$version
-done
+ant generate-test-graph -Dneo4j.version=$1
